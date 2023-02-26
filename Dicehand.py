507bc5186f3454e6298ce45429ba74a77cc361d3
Ahmed Radwan <112603804+Ahmedradwancs@users.noreply.github.com>
2023-02-26 08:02:22 +0100

    Update Dicehand.py

--- Dicehand.py
+++ Dicehand.py
@@ -3,7 +3,7 @@ from Dice import Dice
 
 class Dicehand:
     def __init__(self, dice=None):
-        self.dice = dice or [Dice(), Dice()]  # default to two dice
+        self.dice = [Dice(), Dice()]  # default to two dice
         self.num_dice = len(self.dice)
 
     def roll(self):
@@ -22,18 +22,3 @@ class Dicehand:
 
     def __str__(self):
         return f"DiceHand with {self.num_dice} dice: {', '.join(str(die) for die in self.dice)}"
-
-##############################################
-
-    # def __init__(self):
-    #     self.dice = [Dice(), Dice(), Dice(), Dice(), Dice()]
-
-    # def roll_all(self):
-    #     for die in self.dice:
-    #         die.roll()
-
-    # def __str__(self):
-    #     result = ''
-    #     for die in self.dice:
-    #         result = result + str(die) + ' '
-    #     return result
