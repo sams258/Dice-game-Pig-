9c2635a05b01a14e5eb66f4a1c8a3fb4e72b2a2a
Sam <Samsse46@gmail.com>
2023-02-28 09:21:42 +0100

    added str method

--- Dice.py
+++ Dice.py
@@ -1,11 +1,13 @@
 import random
 
 class Dice:
-    def __init__(self, sides):
-        self.sides = sides
+    def __init__(self, faces=6):
+        self.sides = faces
 
     def roll(self):
         return random.randint(1, self.sides)
     
+    def __str__(self):
+        return "A " + str(self.sides) + "-sided dice"
 
 
