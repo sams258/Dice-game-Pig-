26f1dc1c229a3c8f4b3e082c452afff04c5cd214
Sam <115927233+sams258@users.noreply.github.com>
2023-02-28 10:33:10 +0100

    Update Player.py
    added more lines

--- Player.py
+++ Player.py
@@ -1,27 +1,48 @@
 from Dicehand import Dicehand
+from Histogram import Histogram
 
 
 class Player:
-
-    def __init__(self, name):
+    def __init__(self, name, intelligence=None):
         self.name = name
         self.score = 0
-        self.hand = Dicehand()
-
-    def roll_dice(self):
-        self.hand.roll_all()
-
-    def select_dice(self, dice_indices):
-        selected_dice = []
-        for i in dice_indices:
-            selected_dice.append(self.hand.dice[i])
-            return selected_dice
-
-    def get_score(self):
-        return self.score
-
-    def set_score(self, new_score):
-        self.score = new_score
+        self.intelligence = intelligence
 
     def __str__(self):
-        return f"Player {self.name} with score {self.score} and hand: {self.hand}"
+        return f"{self.name} ({self.score})"
+
+    def play(self, opponent_score, high_scores):
+        print(f"{self.name}'s turn")
+        print(f"{self.name} score: {self.score}")
+        print(f"Opponent score: {opponent_score}")
+        dice_hand = Dicehand()
+        turn_score = 0
+        while True:
+            action = None
+            if self.intelligence:
+                action = self.intelligence.choose_action(self.score, opponent_score)
+            else:
+                while action not in ["roll", "hold"]:
+                    action = input("Roll or hold? ").lower()
+            if action == "roll":
+                dice = dice_hand.roll()
+                print(f"{self.name} rolls: {dice}")
+                hist = Histogram(dice)
+                print(hist)
+                if 1 in dice:
+                    print("Pig! No points earned.")
+                    turn_score = 0
+                    break
+                else:
+                    turn_score += sum(dice)
+            else:
+                break
+        self.score += turn_score
+        print(f"{self.name} ends turn with {turn_score} points")
+        print(f"{self.name} total score: {self.score}")
+        if self.score >= 100:
+            print(f"{self.name} wins!")
+            high_scores.add_score(self.name, self.score)
+            return True
+        else:
+            return False
