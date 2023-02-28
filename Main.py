3219d473057253e46b6caf5b5c229db09010cc13
Ahmed Radwan <112603804+Ahmedradwancs@users.noreply.github.com>
2023-02-28 10:42:32 +0100

    Update Main.py
    Adding the Main class code

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+from Dice import Dice
+from Dicehand import Dicehand
+from Player import Player
+from Histogram import Histogram
+from Highscore import Highscore
+from Intelligence import Intelligence
+from Game import Game
 
+
+class Main:
+  
+    high_scores = Highscore()
+    player1 = Player("Alice")
+    player2 = Player("Bob", Intelligence("medium"))
+    game = Game(player1, player2, high_scores)
+    game.play()
+    
