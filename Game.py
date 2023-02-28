b48c0d58b08957632d4323cd505743a406d81b0f
Sam <Samsse46@gmail.com>
2023-02-28 11:03:28 +0100

    option to quit game

--- Game.py
+++ Game.py
@@ -13,6 +13,31 @@ class Game:
             if self.player2.play(self.player1.score, self.high_scores):
                 break
 
+            # Prompt the user to continue playing or exit the game
+            choice = input("Press 'c' to continue playing, or 'q' to exit the game: ")
+            if choice.lower() == 'q':
+                break
+
         print("High Scores:")
         for name, score in self.high_scores.get_high_scores():
             print(f"{name}: {score}")
+
+
+# class Game:
+#     def __init__(self, player1, player2, high_scores):
+#         self.player1 = player1
+#         self.player2 = player2
+#         self.high_scores = high_scores
+
+#     def play(self):
+#         print("Welcome to Pig!")
+#         print(f"{self.player1.name} vs. {self.player2.name}")
+#         while True:
+#             if self.player1.play(self.player2.score, self.high_scores):
+#                 break
+#             if self.player2.play(self.player1.score, self.high_scores):
+#                 break
+
+#         print("High Scores:")
+#         for name, score in self.high_scores.get_high_scores():
+#             print(f"{name}: {score}")
