e4c11ebe473ff21877489c1f3d9b2c2adad5c34d
Sam <115927233+sams258@users.noreply.github.com>
2023-02-28 10:38:28 +0100

    Update Dicehand.py
    removed unused lines

--- Dicehand.py
+++ Dicehand.py
@@ -1,24 +1,10 @@
 from Dice import Dice
 
-
 class Dicehand:
-    def __init__(self, dice=None):
-        self.dice = [Dice(), Dice()]  # default to two dice
-        self.num_dice = len(self.dice)
+    def __init__(self, num_dice=1, num_sides=6):
+        self.dice = [Dice(num_sides) for _ in range(num_dice)]
+        self.values = []
 
     def roll(self):
-        for die in self.dice:
-            die.roll()
-
-    def add_dice(self, num_dice=1, num_sides=6):
-        for i in range(num_dice):
-            self.dice.append(Dice(num_sides))
-        self.num_dice = len(self.dice)
-
-    def remove_dice(self, num_dice=1):
-        for i in range(num_dice):
-            self.dice.pop()
-        self.num_dice = len(self.dice)
-
-    def __str__(self):
-        return f"DiceHand with {self.num_dice} dice: {', '.join(str(die) for die in self.dice)}"
+        self.values = [die.roll() for die in self.dice]
+        return self.values
