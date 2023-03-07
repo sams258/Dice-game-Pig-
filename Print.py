6fc6db035dad5d2a7ac9e9eb14dd3db40ace9859
Sam <115927233+sams258@users.noreply.github.com>
2023-03-07 14:13:58 +0100

    Update Print.py
    added the method print_sleep that delays printing and adds an empty line

--- Print.py
+++ Print.py
@@ -1,1 +1,10 @@
+import time
 
+class Print:
+    def __init__(self):
+        pass
+    
+    def print_sleep(*args, **kwargs):
+        print(*args, **kwargs)
+        print()
+        time.sleep(1)
