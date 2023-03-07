a6c5e0d3b02f2a16fed68e82d96168a86f9f3397
Sam <115927233+sams258@users.noreply.github.com>
2023-03-07 14:07:43 +0100

    Update ComputerPlayer.py
    Updated the game rules for the Computer player

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -1,11 +1,48 @@
-class Intelligence:
-    def __init__(self, difficulty):
+from Player import Player
+from Print import Print
+from Histogram import Histogram
+import random
+
+class ComputerPlayer(Player):
+    def __init__(self, name, difficulty):
+        super().__init__(name)
         self.difficulty = difficulty
 
-    def choose_action(self, score, opponent_score):
-        if self.difficulty == "easy":
-            return "roll" if score < 20 else "hold"
-        elif self.difficulty == "medium":
-            return "roll" if score < 25 or score < opponent_score else "hold"
-        elif self.difficulty == "hard":
-            return "roll" if score < 30 or score < opponent_score else "hold"
+    def take_turn(self):
+        turn_score = 0
+        while True:
+            
+            roll = random.randint(1, 6)
+            dice_faces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+            Print.print_sleep(f"{self.name} rolled a {[roll]} {dice_faces[roll-1]}")
+            
+            Histogram.add_roll(roll)
+            if roll == 1:
+                Print.print_sleep(f"{self.name} lost their turn!")
+                
+                return 0
+            else:
+                turn_score += roll
+                Print.print_sleep(f"{self.name}'s current turn score: {turn_score}")
+                
+                if self.difficulty == "easy":
+                    if turn_score >= 10:
+                        self.total_score += turn_score
+                        Print.print_sleep(f"{self.name}'s total score: {self.total_score}")
+                        
+                        return turn_score
+                elif self.difficulty == "hard":
+                    if turn_score >= (15 - len(str(self.total_score))):
+                        self.total_score += turn_score
+                        Print.print_sleep(f"{self.name}'s total score: {self.total_score}")
+                        
+                        return turn_score
+                else:
+                    # choice = input("Do you want to roll again? (y/n) ")
+                    # if choice.lower() == "n":
+                        
+                        self.total_score += turn_score
+                        Print.print_sleep(f"{self.name}'s total score: {self.total_score}")
+                        
+                        return turn_score
+
