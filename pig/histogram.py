05e85493457701b8096410fdae90686ed65ab3d2
Ahmed Radwan <112603804+Ahmedradwancs@users.noreply.github.com>
2023-03-10 12:49:31 +0100

    Update histogram.py
    Updating docstrings for Histogram class

--- pig/histogram.py
+++ pig/histogram.py
@@ -4,32 +4,41 @@ from print import Print
 
 
 class Histogram:
-    """_summary_."""
+    """Represents a histogram of rolls in the game of Pig.
+
+    This class provides methods for adding rolls to the histogram,
+    clearing the histogram, displaying the histogram, and setting a cheat
+    mode where the histogram is fixed to always roll a 6.
+
+    Attributes:
+        rolls (dict): A dictionary mapping each possible roll (1-6) to the
+            number of times it has been rolled.
+    """
 
     rolls = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
     @classmethod
     def add_roll(cls, roll):
-        """_summary_.
+        """Add a roll to the histogram.
 
         Args:
-            roll (_type_): _description_
+            roll (int): The roll value to add to the histogram.
         """
         cls.rolls[roll] += 1
 
     @classmethod
     def clear(cls):
-        """_summary_."""
+        """Clear the histogram."""
         cls.rolls = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
     @classmethod
     def display(cls):
-        """_summary_."""
+        """Display the histogram."""
         Print.print_sleep("\nHistogram of Rolls:")
         for roll, count in cls.rolls.items():
             print(f"{roll}: {'*' * count}")
 
     @classmethod
     def set_cheat(cls):
-        """_summary_."""
+        """Set the histogram to cheat mode, always rolling a 6."""
         cls.rolls = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 1}
